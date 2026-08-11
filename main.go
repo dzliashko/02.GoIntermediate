c98aaa4c24b04c2c4f4081358c119bdca0cfd29c
Dzmitry Liashko <dzliashko@gmail.com>
2026-08-11 15:27:01 +0300

    done

--- main.go
+++ main.go
@@ -1,16 +1,35 @@
 package main
-import ("bufio"; "fmt"; "os"; "strconv")
 
-// type Shape interface { ... }
-// type Circle struct { ... }
-// func (c Circle) Area() float64 { ... }
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+)
+
+type Shape interface{ Area() float64 }
+type Circle struct{ Radius float64 }
+type Square struct{ Size float64 }
+
+func (c Circle) Area() float64 { return 3.14 * c.Radius * c.Radius }
+func (c Square) Area() float64 { return c.Size * c.Size }
 
 func main() {
-    sc := bufio.NewScanner(os.Stdin)
-    sc.Scan(); kind := sc.Text()
-    sc.Scan(); dim, _ := strconv.ParseFloat(sc.Text(), 64)
-    var s interface{ Area() float64 }
-    _ = kind; _ = dim
-    // s = ... based on kind
-    if s != nil { fmt.Printf("%.2f\n", s.Area()) }
+	sc := bufio.NewScanner(os.Stdin)
+	sc.Scan()
+	kind := sc.Text()
+	sc.Scan()
+	dim, _ := strconv.ParseFloat(sc.Text(), 64)
+	var s Shape
+
+	switch kind {
+	case "circle":
+		s = Circle{Radius: dim}
+	case "square":
+		s = Square{Size: dim}
+	}
+
+	if s != nil {
+		fmt.Printf("%.2f\n", s.Area())
+	}
 }
